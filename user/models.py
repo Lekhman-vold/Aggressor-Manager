4f529779e672aa2b24ca25d18332055297ecc495
Lekhman-vold <volodya.l@yahoo.com>
2021-05-16 22:03:18 +0300

    update user model

--- user/models.py
+++ user/models.py
@@ -4,3 +4,8 @@ from django.contrib.auth.models import User
 
 class UserModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    work_status = models.BooleanField(default=False)
+    user_description = models.CharField(max_length=1000, blank=True)
+
+    def __str__(self):
+        return f'Username: {self.user}, Work status: {self.work_status}'
