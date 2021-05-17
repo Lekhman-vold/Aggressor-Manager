2cb722c5e6e165e0841c67b20fb1e321a4922660
Lekhman-vold <volodya.l@yahoo.com>
2021-05-18 00:48:02 +0300

    add login in app

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import user_profile
+from django.contrib.auth import views
+from .views import user_profile, user_login
 
 
 urlpatterns = [
-    path('profile/', user_profile)
+    path('login/', user_login, name='login'),
+    # path('login/', views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('profile/', user_profile),
 ]
