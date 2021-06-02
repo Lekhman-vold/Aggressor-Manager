2c8639cd76ff69bbc4a660e37a2d439add0a4f5d
Lekhman-vold <volodya.l@yahoo.com>
2021-06-02 23:30:56 +0300

    add grappelli

--- manager_agressor/urls.py
+++ manager_agressor/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),  # grappelli URLS
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
     path('user/', include('user.urls')),
